7881a477d41dfda8577b7a69022e65bcdd299ef7
rebeca4692 <martharebecagtz@gmail.com>
2015-04-02 01:38:42 -0600

    First version of models

--- hierarchy/models.py
+++ hierarchy/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Teams(models.Model):
+    name = models.CharField("Team name", max_length=140)
+    logo = models.CharField("Team Logo", max_length=140, blank=True)
+    active = models.BooleanField("Active", default=True)
+
+    class Meta:
+        permissions = (
+        )
+
+    def __unicode__(self):
+        return self.name
+
+
+class TeamMembers(models.Model):
+    user = models.ForeignKey("basic.Users", verbose_name="User")
+    team = models.ForeignKey("Teams", verbose_name="Team")
+    active = models.BooleanField("Active", default=True)
+    class Meta:
+        permissions = (
+        )
+
+    def __unicode__(self):
+        return "%s - %s" % (self.user.name, self.team.name)
+
+
+class Roles(models.Model):
+    name = models.CharField("Role name", max_length=140)
+    team = models.ForeignKey("Teams", verbose_name="Team")
+    active = models.BooleanField("Active", default=True)
+
+    class Meta:
+        permissions = (
+        )
+
+    def __unicode__(self):
+        return self.name
--- workflow/models.py
+++ workflow/models.py
@@ -1,3 +1,79 @@
 from django.db import models
 
-# Create your models here.
+
+class Process(models.Model):
+    name = models.CharField("Process Name", max_length=140)
+    description = models.CharField("Description", max_length=300)
+    parent_id = models.ForeignKey("Process", verbose_name="Parent Process", null=True)
+    active = models.BooleanField("Active", default=True)
+
+    class Meta:
+        permissions = (
+        )
+
+    def __unicode__(self):
+        return self.name
+
+
+class Stage(models.Model):
+    name = models.CharField("Stage Name", max_length=100)
+    description = models.CharField("Stage Description", max_length=200)
+    responsible = models.IntegerField("Responsible")
+    process = models.ForeignKey("Process", verbose_name="Process")
+    active = models.BooleanField("Active", default=True)
+
+    class Meta:
+        permissions = (
+
+        )
+
+    def __unicode__(self):
+        return self.name
+
+
+class Task(models.Model):
+    description = models.CharField("Task", max_length=300)
+    responsible = models.IntegerField("Responsible")
+    completedBy = models.IntegerField("Completed By")
+    stage = models.ForeignKey("Stage", verbose_name="Stage")
+    active = models.BooleanField("Active", default=True)
+
+    class Meta:
+        permission = (
+
+        )
+
+    def __unicode__(self):
+        return self.description
+
+
+class StageMessage(models.Model):
+    message = models.CharField("Message", max_length=500)
+    parent_id = models.IntegerField("In reply to")
+    stage = models.ForeignKey("Stage", verbose_name="Stage")
+    reply_to = models.ForeignKey("StageMessage", verbose_name="Reply to", null=True)
+    active = models.BooleanField("Active", default=True)
+
+    class Meta:
+        permissions = (
+
+        )
+
+    def __unicode__(self):
+        return self.message
+
+
+class TaskMessage(models.Model):
+    message = models.CharField("Message", max_length=500)
+    parent_id = models.IntegerField("In reply to")
+    task = models.ForeignKey("Task", verbose_name="Task")
+    reply_to = models.ForeignKey("TaskMessage", verbose_name="Reply to", null=True)
+    active = models.BooleanField("Active", default=True)
+
+    class Meta:
+        permissions = (
+
+        )
+
+    def __unicode__(self):
+        return self.message
