c90b2dd63bcc923ce81f08006de7b7cd60cfb63d
rebeca4692 <martharebecagtz@gmail.com>
2015-05-07 11:11:51 -0500

    add member to teams with roles

--- hierarchy/forms.py
+++ hierarchy/forms.py
@@ -2,10 +2,10 @@ __author__ = 'Rebeca'
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from basic.models import User
 from django.core.exceptions import ValidationError
 from django.forms import ModelForm
-from .models import Team, TeamMember, Role
+from .models import Team, TeamMember, Role, TeamRole
 
 
 class TeamForm(ModelForm):
@@ -25,3 +25,24 @@ class RoleForm(ModelForm):
     class Meta:
         model = Role
         exclude = ('active',)
+
+
+
+class TeamRoleForm(ModelForm):
+    user = forms.CharField(label='User', max_length=100)
+
+    def clean(self):
+        cleaned_data = super(TeamRoleForm, self).clean()
+        print cleaned_data
+        username = cleaned_data.get("user")
+        user = User.objects.all().filter(user__username=username)
+        print user
+        if not user.count():
+            message = u"User does not exist" 
+            self.add_error('user', message)
+
+        return self.cleaned_data
+
+    class Meta:
+        model = TeamRole
+        exclude = ('active', 'user')
